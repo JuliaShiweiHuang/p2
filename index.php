1ea09bcd336b5ffd7c19ed62b3ee3d38b21b58fa
Shiwei Huang <shiwei.julia.huang@gmail.com>
2016-02-25 00:34:17 -0500

    fixing URL refresh issue

--- index.php
+++ index.php
@@ -17,36 +17,43 @@
             <p class ="displayPassword">
 				<!-- using a for loop to generate password based on customer's "number of words" input -->
                 <?php
-                for ($i = 0; $i < $numOfWords-1; $i++) {
-                    echo $wordArray[$randWordArrayIndex];
-                    $randWordArrayIndex = rand(0, $wordArrayLength-1);
-                    echo "-";
-                }
-                echo $wordArray[$randWordArrayIndex];
+				if ($numOfWords < 9 ) { // added server side validation for the user input;
+	                for ($i = 0; $i < $numOfWords-1; $i++) {
+	                    echo $wordArray[$randWordArrayIndex];
+	                    $randWordArrayIndex = rand(0, $wordArrayLength-1);
+	                    echo "-";
+	                }
+	                echo $wordArray[$randWordArrayIndex];
 
-				if ($_GET['add_number'] == 'on') { # if checkbox is marked for "on";
-					echo rand(0, 99); # generate a random number between 0 and 99;
-				}
+					if ($_GET['add_number'] == 'on') { # if checkbox is marked for "on";
+						echo rand(0, 99); # generate a random number between 0 and 99;
+					}
 
-				if ($_GET['add_symbol'] == 'on') { # if checkbox is marked for "on" option;
-					echo $symbolArray[$randSymbolArrayIndex]; # generate a '@' symbol;
-				}
+					if ($_GET['add_symbol'] == 'on') { # if checkbox is marked for "on" option;
+						echo $symbolArray[$randSymbolArrayIndex]; # generate a '@' symbol;
+					}
+				}//if
                 ?>
             </p>
 
 
 		<form action ='index.php' method='GET'>
 			<p class='options'>
-
 				<label for='number_of_words'># of Words</label>
-				<input maxlength=1 type='text' name='number_of_words' id='number_of_words' >  (Max 9)
+				<input maxlength=1 type='text' name='number_of_words' id='number_of_words'
+				<?php if(isset($numOfWords)){echo "value=\"".$numOfWords."\" ";} ?>>  (Max 9)
 				<br>
 
-				<input type='checkbox' name='add_number' id='add_number' value='on'>
+				<input type='checkbox' name='add_number' id='add_number' value='on'
+				<?php if(isset($_GET['add_number']) && $_GET['add_number'] == 'on'){echo "checked=\"checked\" ";} ?>>
 				<label for='add_number'>Add a number</label>
 				<br>
-				<input type='checkbox' name='add_symbol' id='add_symbol' value='on' >
+
+				<input type='checkbox' name='add_symbol' id='add_symbol' value='on'
+				<?php if(isset($_GET['add_symbol']) && $_GET['add_symbol'] == 'on'){echo "checked=\"checked\" ";} ?>>
 				<label for='add_symbol'>Add a symbol</label>
+
+
 			</p>
 
             <div class='buttonHolder'>
@@ -54,11 +61,18 @@
 				<br>
 
 				<p style ="color: red;">
+					<!-- WARNING: ERROR STATEMENT IS STILL NOT WORKING. -->
 					<!-- Question: if a user types a character, this function doesn't work though. Any suggestions? -->
 					<?php
-						if ($_GET['number_of_words'] != (1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9) ) {
-							echo "Invalid symbol entered. Please enter a number between 1 and 9";
+						if ( !isset($_GET['number_of_words']) &&
+							 !is_numeric($_GET['number_of_words'])
+						){
+						echo "Invalid symbol entered. Please enter a number between 1 and 9";
 						}
+
+						// if ($_GET['number_of_words'] != (1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9) ) {
+						// 	echo "Invalid symbol entered. Please enter a number between 1 and 9";
+						// }
 					?>
 				</p>
                 <br><!--Question: not sure whether this is good practice. But I want to separate the 'Gimme Another button with the comics' -->
